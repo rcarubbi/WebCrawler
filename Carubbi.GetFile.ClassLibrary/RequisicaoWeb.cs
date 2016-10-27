dfe27f1b720654ff20e57d0d1a6890c59cdb36fc
Raphael Carubbi Neto <raphael@gmail.com>
2016-10-26 22:17:28 -0200

    Commit Inicial

---
+++ Carubbi.GetFile.ClassLibrary/RequisicaoWeb.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Carubbi.GetFile.ClassLibrary
+{
+    public class RequisicaoWeb
+    {
+        private HttpWebRequest _request;
+        private BinaryWriter _writer;
+
+        public RequisicaoWeb(string url, string destino, int indicePasta, int indiceArquivo)
+        {
+            Url = url;
+            Destino = destino;
+            IndicePasta = indicePasta;
+            IndiceArquivo = indiceArquivo;
+
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        public string Destino
+        {
+            get;
+            set;
+        }
+
+        public int IndicePasta
+        {
+            get;
+            set;
+        }
+
+        public int IndiceArquivo
+        {
+            get;
+            set;
+        }
+
+     
+
+        public void Requisitar()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(ResolverCaminho(Url, IndicePasta, IndiceArquivo));
+           _request.Method = "GET";
+           _request.ContentType = "image/jpeg";
+           WebResponse _response = _request.GetResponse();
+           GravarArquivo(_response.GetResponseStream(), Destino, IndicePasta, IndiceArquivo);
+        }
+
+        
+
+
+        private string ResolverCaminho(string caminho, int indicePasta, int indiceArquivo)
+        {
+            string caminhoRetorno = string.Empty;
+            caminhoRetorno = caminho.Replace("[1]", indicePasta.ToString()).Replace("[2]", indiceArquivo.ToString());
+            return caminhoRetorno;
+        }
+
+        private void GravarArquivo(Stream dados, string Destino, int indicePasta, int indiceArquivo)
+        {
+            string caminho = ResolverCaminho(Destino, indicePasta, indiceArquivo);
+
+            string pasta = Path.GetDirectoryName(caminho);
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            if (!File.Exists(caminho))
+            {
+                FileStream fs = File.Create(caminho);
+                _writer = new BinaryWriter(fs);
+                int Length = 256;
+                Byte[] buffer = new Byte[Length];
+                int bytesRead = dados.Read(buffer, 0, Length);
+                // write the required bytes
+                while (bytesRead > 0)
+                {
+                    _writer.Write(buffer, 0, bytesRead);
+                    bytesRead = dados.Read(buffer, 0, Length);
+                }
+                _writer.Close();
+                fs.Close();
+            }
+        
+        }
+
+    }
+}
---
+++ Carubbi.GetFile.UI/Form1.cs
@@ -0,0 +1,130 @@
+﻿using Carubbi.GetFile.ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Carubbi.GetFile.UI
+{
+    public partial class frmGetFile : Form
+    {
+        private static ManualResetEvent[] resetEvents;
+
+        private int _indiceArquivo;
+        private int _indicePasta;
+        private bool hasFiles = true;
+        public frmGetFile()
+        {
+            InitializeComponent();
+            
+        }
+      
+
+
+        private void btnRequisitar_Click(object sender, EventArgs e)
+        {
+
+            ThreadPool.SetMaxThreads(5, 5);
+            
+            int pastaDe = Convert.ToInt32(txtPastaDe.Text);
+            int availableThreadsCount, completionPortThreads;
+          
+            _indicePasta = pastaDe;
+            _indiceArquivo = 1;
+            while (hasFiles)
+            {
+                FlushLog();
+                ThreadPool.GetAvailableThreads(out availableThreadsCount, out completionPortThreads);
+                if (availableThreadsCount > 0)
+                {
+                    WaitCallback callBack = new WaitCallback(CallBackRequisicao);
+                    ThreadPool.QueueUserWorkItem(CallBackRequisicao, new object[] { _indiceArquivo++, _indicePasta, txtUrl.Text, txtDestino.Text});
+                }
+                Thread.Sleep(500);
+            }
+
+
+            //    
+            //while(hasFiles)
+            //{
+            //    int availableThreadsCount, completionPortThreads;
+            //    ThreadPool.GetAvailableThreads(out availableThreadsCount, out completionPortThreads);
+                 
+            //    if (availableThreadsCount > 0)
+            //    {
+            //       resetEvents[availableThreadsCount-1] = new ManualResetEvent(false);
+            //       WaitCallback callBack = new WaitCallback(CallBackRequisicao);
+            //       ThreadPool.QueueUserWorkItem(CallBackRequisicao, _indiceArquivo++);
+            //       Thread.Sleep(999999999);
+            //    }
+                
+            //}
+         
+
+            
+            AddLog("------- Requisição concluída ------");
+            
+        }
+
+        void CallBackRequisicao(object parameters)
+        {
+
+            int indiceArquivo = (int)((object[])parameters)[0];
+            int indicePasta = (int)((object[])parameters)[1];
+            string url = (string)((object[])parameters)[2];
+            string destino = (string)((object[])parameters)[3];
+            try
+            {
+                RequisicaoWeb requisicao = new RequisicaoWeb(url, destino, indicePasta, indiceArquivo);
+                requisicao.Requisitar();
+                AddLog(string.Format("pasta {0} - arquivo {1} gravado com sucesso", indicePasta, indiceArquivo));
+            }
+            catch
+            {
+                AddLog(string.Format(" ------ Proxima pasta - {0} -------", indicePasta + 1));
+                Interlocked.Increment(ref _indicePasta);
+                int pastaAte = Convert.ToInt32(txtPastaAte.Text);
+                if (_indicePasta > pastaAte)
+                {
+                    lock (this)
+                    {
+                        hasFiles = false;
+                    }
+                }
+            }
+        }
+
+
+        List<string> Log = new List<string>();
+        private void AddLog(string mensagem)
+        {
+            Log.Add(mensagem);
+        }
+
+        private void FlushLog()
+        {
+            foreach (string mensagem in Log)
+                lstLog.Items.Insert(0, mensagem);
+        }
+
+      
+        #region hiddenFeatures
+        bool muda = false;
+        private void lblLinks_DoubleClick(object sender, EventArgs e)
+        {
+            txtDestino.Text = @"C:\teste\[1]\[2].jpg";
+            if (muda)
+            {
+
+                txtUrl.Text = "http://www.cnnamador.com/img/[1]/photos/[2].jpg";
+            }
+            else
+            {
+                txtUrl.Text = "Http://www.qualitypage.com/asians/[1]/[2].jpg";
+            }
+            muda = !muda;
+        }
+        #endregion
+
+    }
+}
